b797200e857efd551ec89c6ab2a8e47b206ba221
denisrtorres <denisrtorres18@hotmail.com>
2026-07-02 21:45:36 -0300

    lista de exercícios

---
+++ ex1.py
@@ -0,0 +1,15 @@
+#Analise de Vendas
+#Considere a tupla:
+#Faça um programa que exiba
+#a) a maior venda.
+#b) a menor venda.
+#c) calcule a média das vendas.
+#d) informe quantas vendas ficaram acima da média.
+vendas = (1500, 2300, 1800, 2900, 3200, 2100, 1700)
+
+maior_venda = max(vendas)
+
+menor_venda = min(vendas)
+
+media_vendas = sum(vendas) / len(vendas)
+
