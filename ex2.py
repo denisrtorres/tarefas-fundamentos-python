0c08f6be67f76caf652c4acaf7eab1afa4064adf
denisrtorres <denisrtorres18@hotmail.com>
2026-07-02 21:46:57 -0300

    exercício 2, parte 1

---
+++ ex2.py
@@ -0,0 +1,12 @@
+#Dada a tupla: numeros = (5, 2, 8, 2, 9, 2, 5, 7, 8, 5)
+#Crie um programa que:
+#a) Descubra todos os números que aparecem mais de uma vez.
+#b) Mostre quantas vezes cada um deles aparece.
+#Exemplo de saída
+#5 aparece 3 vezez
+#2 aparece 3 vezes
+#8 aparece 2 vezes
+
+numeros = (5, 2, 8, 2, 9, 2, 5, 7, 8, 5)
+
+exibidos = set()
